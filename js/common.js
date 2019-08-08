117d5f9e2249c3c7923b645c23c20638596bd1ca
KazukiYoza <52521949+KazukiYoza@users.noreply.github.com>
2019-08-09 01:07:10 +0900

    effect-fade add

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// ロード画面グリッド
 function grid(){
     $(".g1").animate({'background-color':'#ffffff'},1500);
     $(".g2").animate({'background-color':'#ffffff'},900);
@@ -13,7 +14,7 @@ function grid(){
 };
 setTimeout(grid,1000);
 
-
+// ヘッダースライド
 function slide(){
     $(".front-layer").animate({'left': '0%'},900);
     $(".back-layer").animate({'right': '0%'},500);
@@ -22,7 +23,7 @@ function slide(){
 };
 setTimeout(slide, 3500);
 
-
+// スクロールライン
 function line(){
     winW = $('.scroll-navi').width();
     spped = 4000;
@@ -38,6 +39,7 @@ function line(){
 }
 line();
 
+// ボーダーの線が動く
 $(window).on('scroll', function (){
     // var move = $('.moving-border');
     // move.each(function () {
@@ -55,206 +57,9 @@ $(window).on('scroll', function (){
         $('.moving-border .container').removeClass('sphover');
         console.log("clear2")
         }
-    });
-  
-//   });
+    });  
 
-// エフェクト1
-(function() {
-    setTimeout(init, 10);
-        function init() {
-            var scrollElemToWatch_1 = document.getElementById('effect_01'),
-                    watcher_1 = scrollMonitor.create(scrollElemToWatch_1, -10),				
-                    rev1 = new RevealFx(scrollElemToWatch_1, {
-                        revealSettings : {
-                            bgcolor: '#182524',
-                            duration: 300,
-                            onStart: function(contentEl, revealerEl) { 
-                                anime.remove(contentEl);
-                                contentEl.style.opacity = 0; 
-                            },
-                            onCover: function(contentEl, revealerEl) { 
-                                contentEl.style.opacity = 1;
-                                anime({
-                                    targets: contentEl,
-                                    easing: 'easeOutExpo'
-                                });
-                            }
-                        }
-                    })
-            watcher_1.enterViewport(function() {
-                rev1.reveal();
-                watcher_1.destroy();
-            });
-        }
-    })();
-// エフェクト2
-(function() {
-    setTimeout(init, 10);
-        function init() {
-            var scrollElemToWatch_2 = document.getElementById('effect_02'),
-                    watcher_2 = scrollMonitor.create(scrollElemToWatch_2, -10),				
-                    rev2 = new RevealFx(scrollElemToWatch_2, {
-                        revealSettings : {
-                            bgcolor: '#182524',
-                            duration: 300,
-                            onStart: function(contentEl, revealerEl) { 
-                                anime.remove(contentEl);
-                                contentEl.style.opacity = 0; 
-                            },
-                            onCover: function(contentEl, revealerEl) { 
-                                contentEl.style.opacity = 1;
-                                anime({
-                                    targets: contentEl,
-                                    easing: 'easeOutExpo'
-                                });
-                            }
-                        }
-                    })               
-                    watcher_2.enterViewport(function() {
-                rev2.reveal();
-                watcher_2.destroy();
-            });
-        }
-    })();
-// エフェクト3
-(function() {
-    setTimeout(init, 10);
-        function init() {
-            var scrollElemToWatch_3 = document.getElementById('effect_03'),
-                    watcher_3 = scrollMonitor.create(scrollElemToWatch_3, -10),				
-                    rev3 = new RevealFx(scrollElemToWatch_3, {
-                        revealSettings : {
-                            bgcolor: '#182524',
-                            duration: 300,
-                            onStart: function(contentEl, revealerEl) { 
-                                anime.remove(contentEl);
-                                contentEl.style.opacity = 0; 
-                            },
-                            onCover: function(contentEl, revealerEl) { 
-                                contentEl.style.opacity = 1;
-                                anime({
-                                    targets: contentEl,
-                                    easing: 'easeOutExpo'
-                                });
-                            }
-                        }
-                    })               
-                    watcher_3.enterViewport(function() {
-                rev3.reveal();
-                watcher_3.destroy();
-            });
-        }
-    })();
-// エフェクト4
-(function() {
-    setTimeout(init, 10);
-        function init() {
-            var scrollElemToWatch_4 = document.getElementById('effect_04'),
-                    watcher_4 = scrollMonitor.create(scrollElemToWatch_4, -10),				
-                    rev4 = new RevealFx(scrollElemToWatch_4, {
-                        revealSettings : {
-                            bgcolor: '#182524',
-                            duration: 300,
-                            onStart: function(contentEl, revealerEl) { 
-                                anime.remove(contentEl);
-                                contentEl.style.opacity = 0; 
-                            },
-                            onCover: function(contentEl, revealerEl) { 
-                                contentEl.style.opacity = 1;
-                                anime({
-                                    targets: contentEl,
-                                    easing: 'easeOutExpo'
-                                });
-                            }
-                        }
-                    })               
-                    watcher_4.enterViewport(function() {
-                rev4.reveal();
-                watcher_4.destroy();
-            });
-        }
-    })();
-// エフェクト5
-        (function() {
-            setTimeout(init, 10);
-                function init() {
-                    var scrollElemToWatch_5 = document.getElementById('effect_05'),
-                            watcher_5 = scrollMonitor.create(scrollElemToWatch_5, -10),				
-                            rev5 = new RevealFx(scrollElemToWatch_5, {
-                                revealSettings : {
-                                    bgcolor: '#182524',
-                                    duration: 300,
-                                    onStart: function(contentEl, revealerEl) { 
-                                        anime.remove(contentEl);
-                                        contentEl.style.opacity = 0; 
-                                    },
-                                    onCover: function(contentEl, revealerEl) { 
-                                        contentEl.style.opacity = 1;
-                                        anime({
-                                            targets: contentEl,
-                                            easing: 'easeOutExpo'
-                                        });
-                                    }
-                                }
-                            })               
-                            watcher_5.enterViewport(function() {
-                        rev5.reveal();
-                        watcher_5.destroy();
-                    });
-                }
-            })();
-// エフェクト6
-(function() {
-    setTimeout(init, 10);
-        function init() {
-            var scrollElemToWatch_6 = document.getElementById('effect_06'),
-                    watcher_6 = scrollMonitor.create(scrollElemToWatch_6, -10),				
-                    rev6 = new RevealFx(scrollElemToWatch_6, {
-                        revealSettings : {
-                            bgcolor: '#182524',
-                            duration: 300,
-                            onStart: function(contentEl, revealerEl) { 
-                                anime.remove(contentEl);
-                                contentEl.style.opacity = 0; 
-                            },
-                            onCover: function(contentEl, revealerEl) { 
-                                contentEl.style.opacity = 1;
-                                anime({
-                                    targets: contentEl,
-                                    easing: 'easeOutExpo'
-                                });
-                            }
-                        }
-                    })               
-                    watcher_6.enterViewport(function() {
-                rev6.reveal();
-                watcher_6.destroy();
-            });
-        }
-    })();
-// var timeoutId;
-// var revealElems = document.querySelectorAll('.revealItem');
 
-// window.addEventListener("scroll", function () {
-//     if(timeoutId) return;
-
-//     timeoutId = this.setTimeout( function() {
-//         timeoutId = 0;
-
-//         for (const revealElem of revealElems)  {
-//             const heightOfRevealElem = revealElem.clientHeight;
-//             const revealPoint = (window.scrollY + window.innerHeight) - heightOfRevealElem / 2;
-//             const halfShown = revealPoint > revealElem.offsetTop;
-//             if (halfShown) {
-//                 if (!revealElem.classList.contains('revealActive')) {
-//                     revealElem.className += ' revealActive';
-//                 }
-//             }
-//         };
-
-//     }, 300);
-// });
 
 // フェードイン
 $(function(){
@@ -268,7 +73,7 @@ $(function(){
             }
         });
      });
-    });
+});
     
 // ハンバーガーメニュー
 $(function() {
